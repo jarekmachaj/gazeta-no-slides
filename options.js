217f16f4d72d1ae18c43cc34be8bab583fb1b536
Jarek Machaj <czemuja@gmail.com>
2013-11-12 22:54:25 +0100

    fixing options page

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 var extStatus = {On: "On", Off : "Off"};
-var services = ["avanti24.pl", "bryla.pl", "ciacha.net", "czterykaty.pl", "deser.pl", "domiwnetrze.pl", "domosfera.pl", "e-ogrody.pl", "edziecko.pl", "gazeta.pl", "lula.pl", "moto.pl", "namonciaku.pl", "plotek.pl", "polygamia.pl", "sport.pl", "swiatmotocykli.pl", "technologie.gazeta.pl", "tokfm.pl", "tuba.pl", "ugotuj.to", "wiadomosci.gazeta.pl", "wyborcza.biz", "wyborcza.pl", "wysokieobcasy.pl", "zczuba.pl"]
+var services = ["avanti24.pl", "bryla.pl", "ciacha.net", "czterykaty.pl", "deser.pl", "domiwnetrze.pl", "domosfera.pl", "e-ogrody.pl", "edziecko.pl", "gazeta.pl", "lula.pl", "moto.pl", "namonciaku.pl", "plotek.pl", "polygamia.pl", "sport.pl", "swiatmotocykli.pl", "technologie.gazeta.pl", "tokfm.pl", "tuba.pl", "ugotuj.to", "wiadomosci.gazeta.pl", "wyborcza.biz", "wyborcza.pl", "wysokieobcasy.pl", "zczuba.pl", "gazeta.pl", "lula.pl", "magazyn-kuchnia.pl"]
 var currStatus;
 
 function statusChange(status){
@@ -28,6 +28,13 @@ function saveOptions() {
 }
 
 function restoreOptions() {
+  
+  for (index = 0; index < services.length; ++index) {
+    var content = getSingleStatusString(services[index]);
+    $("#tab1 .well").append(content);
+  }
+
+
   var extcurrstatus;
   chrome.storage.sync.get('noSlidesExtStatus', function(extcurrstatus){
     if (!extcurrstatus) {
@@ -37,6 +44,11 @@ function restoreOptions() {
   });
 }
 
+function getSingleStatusString(service){
+   return '<div class="alert alert-success">' + service + ' <strong><span class="extStatus" id="extStatus">On</span></strong> <div style="display:inline; float:right; clear:both; margin: -5px;" class="btn-group" data-toggle="buttons-radio"> <button id="onBtnStatus" type="button" class="btn btn-success">On</button> <button id="offBtnStatus" type="button" class="btn btn-danger">Off</button> </div></div>'
+}
+
 $("#onBtnStatus").click(function(){statusChange(extStatus.On)});
 $("#offBtnStatus").click(function(){statusChange(extStatus.Off)});
 document.addEventListener('DOMContentLoaded', restoreOptions);
+
